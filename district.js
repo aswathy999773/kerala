46f0c75e27dbad79a301499d415716f676fc5291
aswathy999773 <aswathysuresh251@gmail.com>
2023-06-16 10:34:30 +0530

    first commit

---
+++ district.js
@@ -0,0 +1,102 @@
+let districts=[
+    {
+        id:1,
+        title:"Thiruvananthappuram",
+        desc:"Captital of Kerala",
+        image:"https://dynamic-media-cdn.tripadvisor.com/media/photo-o/09/da/f5/55/kovalam-beach.jpg?w=700&h=-1&s=1"
+    },
+
+    {
+        id:2,
+        title:"Kollam",
+        desc:"Cashew industry of Kerala",
+        image:"https://i0.wp.com/www.tusktravel.com/blog/wp-content/uploads/2023/01/Jatayu-Earth-Centre-Kollam-Kerala.jpg?resize=1024%2C576&ssl=1"
+    },
+
+    {
+        id:3,
+        title:"Pathanamthitta",
+        desc:"Headquarters of Pilgrimage Tourism",
+        image:"https://chaloghumane.com/wp-content/uploads/2021/08/Pathanamthitta-Kerala.jpeg"
+    },
+
+    {
+        id:4,
+        title:"Alappuzha",
+        desc:"City on the Laccadive Sea in the southern Indian state of Kerala.",
+        image:"https://www.mashahur.com/blog/wp-content/uploads/2022/04/backwaters-boating-kerala-india.jpg"
+    },
+
+    {
+        id:5,
+        title:"Kottayam",
+        desc:"Kottayam, IPA: is a city in the Indian state of Kerala. Flanked by the Western Ghats on the east and the Vembanad Lake and paddy fields of Kuttanad on the west. It is the district headquarters of Kottayam district, located in south-west Kerala. ",
+        image:"https://irisholidays.com/keralatourism/wp-content/uploads/2021/05/places-to-visit-in-kottayam.jpg"
+    },
+
+    {
+        id:6,
+        title:"Idukki",
+        desc:"Idukki district is a densely forested, mountainous region in the south Indian state of Kerala. In the north, Anamudi mountain towers over Eravikulam National Park, where the rare, blue Neelakurinji flower blooms every 12 years.",
+        image:"https://images.travelandleisureasia.com/wp-content/uploads/sites/2/2020/11/Feature-image-Idukki.jpg"
+    },
+
+    {
+        id:7,
+        title:"Eranankulam",
+        desc:"Sprawling, residential Ernakulam is known for Marine Drive, a busy waterfront promenade where boats offer backwater cruises.",
+        image:"https://www.oyorooms.com/travel-guide/wp-content/uploads/2019/04/Fort-Kochi.jpg"
+    },
+
+    {
+        id:8,
+        title:"Thrissur",
+        desc:"Known for sacred sites and colorful festivals. ",
+        image:"https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0b/4b/a2/1c/athirapally-falls-in.jpg?w=400&h=300&s=1"
+    },
+
+    {
+        id:9,
+        title:"Palakkad",
+        desc:"Palakkad, or Palghat, is a city in Kerala, a state in southwestern India.",
+        image:"https://www.trawell.in/admin/images/upload/148645431SilentValleyPark_Main.jpg"
+    },
+
+    {
+        id:10,
+        title:"Malapppuram",
+        desc:"Malappuram is a city in the Indian state of Kerala, spread over an area of 158.20 km² including the surrounding suburban areas.",
+        image:"https://tripvibe.online/wp-content/uploads/2021/06/nilambur-1.jpg"
+    },
+
+    {
+        id:11,
+        title:"Kozhikode",
+        desc:"Kozhikode is a coastal city in the south Indian state of Kerala. ",
+        image:"https://i0.wp.com/ctubustimings.com/wp-content/uploads/2021/08/Explore-in-Kozhikode.jpg?fit=900%2C547&ssl=1"
+    },
+
+    {
+        id:12,
+        title:"Wayanad",
+        desc:"Wayanad is a rural district in Kerala state, southwest India.",
+        image:"https://cf.bstatic.com/xdata/images/hotel/max1024x768/332750617.jpg?k=7a0cdce054dabb61f2a5bc082e21a4a85374b58c049f0109c345e7068ca93e55&o=&hp=1"
+    },
+
+    {
+        id:13,
+        title:"Kannur",
+        desc:"It was once an ancient trading port.",
+        image:"https://assets-news.housing.com/news/wp-content/uploads/2022/09/02151433/places-to-visit-in-kannur-shutterstock_1604282707-1200x700-compressed.jpg"
+    },
+
+    {
+        id:14,
+        title:"Kasargod",
+        desc:"The northernmost district of Kerala, Kasaragod is famous for its handlooms and its 293 kilometre long coastline. ",
+        image:"https://static2.tripoto.com/media/filter/tst/img/OgData/1507807944_bekal_fort_kasaragod_kerala_01.jpg"
+    },
+];
+let filteredValue=districts.filter((i) => i.title=="Kasargod")
+console.log(filteredValue)
+
---
+++ script.js
@@ -0,0 +1,16 @@
+
+//  function add(a,b)
+//  {
+//     return a+b;
+
+//  }
+//  console.log(add(8,22))
+ 
+// const add=(a,b)=>a+b
+// console.log(add(30,25))
+
+
+
+const fruits=['apple','orange','banana','orange']
+let filteredValue=fruits.filter((e) => e=='orange')
+console.log(filteredValue)
